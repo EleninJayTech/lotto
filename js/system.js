3757bfe9bd177c321449d09f91de05061dd44c76
elenin <eleninjaytech@gmail.com>
2020-09-30 20:50:57 +0900

    CSS 로드 만들기

--- js/system.js
+++ js/system.js
@@ -2,6 +2,7 @@ let i_System = {
 	loadScriptType:'defer', // 스크립트 로드 타입
 	loadScriptInHead:[], // 스크립트 로드 head
 	loadScript:[], // 스크립트 로드 body
+	loadCss:[], // Css 로드 body
 
 	/**
 	 * 스크립트 HTML 생성
@@ -16,6 +17,18 @@ let i_System = {
 		newScript.setAttribute('crossorigin', 'anonymous');
 		newScript.setAttribute(loadType, '');
 		return newScript;
+	},
+
+	/**
+	 * 스크립트 HTML 생성
+	 * @param href
+	 * @returns {HTMLLinkElement}
+	 */
+	createStyleHtml:function(href){
+		let newStyle = document.createElement('link');
+		newStyle.setAttribute('href', href);
+		newStyle.setAttribute('rel', 'stylesheet');
+		return newStyle;
 	}
 };
 
@@ -23,10 +36,15 @@ let i_System = {
 document.addEventListener("DOMContentLoaded", function(){
 	i_System.loadScriptInHead.push('/lotto/plugin/jquery.min.js');
 	i_System.loadScriptInHead.push('/lotto/plugin/SemanticUI/semantic.min.js');
-
 	i_System.loadScriptInHead.forEach(function(item, index, array){
 		let newScript = i_System.createScriptHtml(item);
 		document.getElementsByTagName('head')[0].appendChild(newScript);
+	});
+
+	i_System.loadCss.push('/lotto/plugin/SemanticUI/semantic.min.css');
+	i_System.loadCss.forEach(function(item, index, array){
+		let newStyle = i_System.createStyleHtml(item);
+		document.getElementsByTagName('head')[0].appendChild(newStyle);
 	});
 });
 
