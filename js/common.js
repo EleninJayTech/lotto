28ddd0b8c3a5b2d034f52b3491a7f1397ae325f6
elenin <eleninjaytech@gmail.com>
2020-10-03 15:32:07 +0900

    로또 API 테스트

--- js/common.js
+++ js/common.js
@@ -5,7 +5,14 @@ let Common = {
 jQuery(function($){
 	console.log('자바 스크립트 로드 테스트');
 
-	$.get('https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=903').done(function(a,b,c,d){
+	$.post('https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=903', {}, function(a,b,c,d,e){
+		console.log('a');
 		console.log(a,b,c,d);
-	});;
+	}).done(function(a,b,c,d){
+		console.log('b');
+		console.log(a,b,c,d);
+	}).fail(function(a,b,c,d){
+		console.log('c');
+		console.log(a,b,c,d);
+	});
 });
